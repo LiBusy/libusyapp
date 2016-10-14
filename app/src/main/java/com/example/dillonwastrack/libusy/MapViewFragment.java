3f67fbc583af9a73632d9e5af9708ecc531c8b98
dillonwastrack <libusyapp@gmail.com>
2016-10-13 19:12:30 -0500

    zoom to user's location, request location permission

--- app/src/main/java/com/example/dillonwastrack/libusy/MapViewFragment.java
+++ app/src/main/java/com/example/dillonwastrack/libusy/MapViewFragment.java
@@ -1,8 +1,17 @@
 package com.example.dillonwastrack.libusy;
 
+import android.*;
+import android.Manifest;
 import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.content.Context.LOCATION_SERVICE;
 
-public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
 
     @Nullable
@@ -32,13 +43,60 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
         fragment.getMapAsync(this);
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         LatLng marker = new LatLng(-33.867, 151.206);
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 13));
+       if(ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+       {
+           ActivityCompat.requestPermissions(this.getActivity(),
+                   new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                   1);
 
-        googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
+       }
+
+
+        googleMap.setMyLocationEnabled(true);
+        zoomToUserLocation(googleMap);
+
+
+    }
+
+    private void zoomToUserLocation(GoogleMap googleMap)
+    {
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location, permission requested in onMapReady
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        //set map type
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Get latitude of the current location
+        double latitude = myLocation.getLatitude();
+
+        // Get longitude of the current location
+        double longitude = myLocation.getLongitude();
+
+        // Create a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // Show the current location in Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+
+        // googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
     }
 }
