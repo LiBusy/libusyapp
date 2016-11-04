9679f1979df0a2552412a7a7676c4b23875389bd
dillonwastrack <libusyapp@gmail.com>
2016-11-03 19:59:52 -0500

    add distance from user to list view

--- app/src/main/java/com/example/dillonwastrack/libusy/adapters/LibraryListAdapter.java
+++ app/src/main/java/com/example/dillonwastrack/libusy/adapters/LibraryListAdapter.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.dillonwastrack.libusy.MainActivity;
 import com.example.dillonwastrack.libusy.R;
 import com.example.dillonwastrack.libusy.models.Library;
 import com.google.android.gms.vision.text.Text;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class LibraryListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
@@ -48,6 +50,12 @@ public class LibraryListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         myHolder.openNow.setText(current.openNow);
         myHolder.busyness.setText(current.busyness);
         myHolder.checkIns.setText(current.checkIns);
+        MainActivity m = (MainActivity) this.context;
+        double distanceInMeters = distance(m.getUserLatLng().latitude,
+                            m.getUserLatLng().longitude,
+                            current.lat,
+                            current.lng);
+        myHolder.distanceAway.setText(String.format(Locale.US,"%.2f meters away", distanceInMeters));
         //myHolder.textType.setText("Category: " + current.catName);
         //myHolder.textPrice.setText("Rs. " + current.price + "\\Kg");
         //myHolder.textPrice.setTextColor(ContextCompat.getColor(context, R.color.colorAccent));
@@ -74,7 +82,7 @@ public class LibraryListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         TextView openNow;
         TextView busyness;
         TextView checkIns;
-       // TextView textType;
+        TextView distanceAway;
         //TextView textPrice;
 
         // create constructor to get widget reference
@@ -85,10 +93,59 @@ public class LibraryListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             openNow = (TextView) itemView.findViewById(R.id.library_open_now);
             busyness = (TextView) itemView.findViewById(R.id.library_busyness);
             checkIns = (TextView) itemView.findViewById(R.id.library_check_ins);
+            distanceAway = (TextView) itemView.findViewById(R.id.library_distance);
             //textType = (TextView) itemView.findViewById(R.id.textType);
             //textPrice = (TextView) itemView.findViewById(R.id.textPrice);
         }
 
+    }
+
+    /**
+     * This distance algorithm uses the haversine
+     * great circle approximation to determine
+     * the distance between two points in
+     * meters.
+     *
+     * @param lat1 latitude of first point
+     * @param lon1 longitude of first point
+     * @param lat2 latitude of second point
+     * @param lon2 longitude of second pount
+     * @return distance between 2 points in meters
+     */
+    private double distance(double lat1, double lon1, double lat2, double lon2)
+    {
+        // haversine great circle distance approximation, returns meters
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60; // 60 nautical miles per degree of separation
+        dist = dist * 1852; // 1852 meters per nautical mile
+        return (dist);
+    }
+
+    /**
+     * Used for converting degrees to
+     * radians.
+     *
+     * @param deg degrees
+     * @return conversion from degrees to radians
+     */
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /**
+     * Used for converting radians to
+     * degrees.
+     *
+     * @param rad radians
+     * @return conversion from radians to degrees
+     */
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
     }
 
 }
